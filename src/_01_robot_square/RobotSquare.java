6966095d78e6dfa681559c8c0f2277f930426096
369906 <369906@0338S-Z-90TDE0R.sdcs.local>
2019-11-12 14:24:11 -0800

    robot grefitti

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,21 +12,48 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
-
+Robot metalica = new Robot();
         // 3. Put the robot's pen down
-
+metalica.penDown();
 
         // 6. Make the robot move as fast as possible
-
+metalica.setSpeed(30);
 
         // 5. Do everything below here 4 times
 
 
         //         2. Move your robot 200 pixels
-
+metalica.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+
+metalica.turn(90);
+metalica.move(200);
+metalica.turn(90);  
+metalica.move(200);
+metalica.turn(90);
+metalica.move(200);
+metalica.move(200);
+metalica.turn(90);    
+metalica.move(200);
+metalica.turn(90);
+metalica.move(200);
+metalica.turn(90);
+metalica.move(200);
+metalica.turn(90);
+metalica.move(200);
+metalica.turn(200);
+metalica.move(200);
+metalica.turn(260);
+metalica.move(75);
+metalica.turn(180);
+metalica.move(75);
+metalica.turn(242);
+metalica.move(250);
+
+
+
+
 
 
     }
